3fd7d83106f627359c670c0a2809d779df459828
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2025-02-12 15:51:41 -0500

    fix response

--- api/index.js
+++ api/index.js
@@ -11,13 +11,23 @@ export default async function handler(req, res) {
     if (req.method !== "POST") return res.status(405).end();
 
     try {
-        const { token } = req.body;
-        const paymentsApi = client.paymentsApi;
 
-        const response = await paymentsApi.createPayment({
-            sourceId: token,
-            idempotencyKey: crypto.randomUUID(),
-            amountMoney: { amount: 500, currency: "USD" }, // $5.00
+        const response = await client.payments.create({
+            idempotencyKey: "7b0f3ec5-086a-4871-8f13-3c81b3875218",
+            amountMoney: {
+                amount: BigInt(500),
+                currency: "USD",
+            },
+            sourceId: "ccof:GaJGNaZa8x4OgDJn4GB",
+            autocomplete: true,
+            customerId: "W92WH6P11H4Z77CTET0RNTGFW8",
+            locationId: "L88917AVBK2S5",
+            referenceId: "123456",
+            note: "Brief description",
+            appFeeMoney: {
+                amount: BigInt(5),
+                currency: "USD",
+            },
         });
 
         res.json({ success: true, payment: response.result });
